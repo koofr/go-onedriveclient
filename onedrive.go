108029f56bb143f62ce003a0595c3c65a2b921ec
Luka Zakrajsek <luka@bancek.net>
2015-06-08 10:37:22 +0200

    Move OneDriveAuth to auth.go

--- onedrive.go
+++ onedrive.go
@@ -1,70 +1,20 @@
 package onedriveclient
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/koofr/go-httpclient"
 	"github.com/koofr/go-ioutils"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path"
 	"strings"
-	"time"
 )
 
 type OneDrive struct {
 	ApiClient     *httpclient.HTTPClient
 	ContentClient *httpclient.HTTPClient
 	Auth          *OneDriveAuth
-}
-
-type OneDriveAuth struct {
-	ClientId     string
-	ClientSecret string
-	RedirectUri  string
-	AccessToken  string
-	RefreshToken string
-	ExpiresAt    time.Time
-}
-
-func (d *OneDriveAuth) ValidToken() (token string, err error) {
-	if time.Now().Unix() > d.ExpiresAt.Unix() {
-		data := url.Values{}
-		data.Set("grant_type", "refresh_token")
-		data.Set("client_id", d.ClientId)
-		data.Set("client_secret", d.ClientSecret)
-		data.Set("redirect_uri", d.RedirectUri)
-		data.Set("refresh_token", d.RefreshToken)
-
-		var resp *http.Response
-
-		resp, err = http.PostForm("https://login.live.com/oauth20_token.srf", data)
-		if err != nil {
-			return
-		}
-
-		if resp.StatusCode != 200 {
-			err = fmt.Errorf("Token refresh failed %d: %s", resp.StatusCode, resp.Status)
-			return
-		}
-
-		var buf []byte
-		if buf, err = ioutil.ReadAll(resp.Body); err != nil {
-			return
-		}
-
-		var respVal RefreshResp
-		if err = json.Unmarshal(buf, &respVal); err != nil {
-			return
-		}
-
-		d.AccessToken = respVal.AccessToken
-		d.ExpiresAt = time.Now().Add(time.Duration(respVal.ExpiresIn) * time.Second)
-	}
-	token = d.AccessToken
-	return
 }
 
 func NewOneDriveClient(auth OneDriveAuth) *OneDrive {
