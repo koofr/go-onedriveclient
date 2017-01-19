6142e25cc3428663113c263a087577ebfe2db39f
Luka Zakrajsek <luka@bancek.net>
2017-01-19 13:42:15 +0100

    Add refresh token callback

--- auth.go
+++ auth.go
@@ -2,10 +2,12 @@ package onedriveclient
 
 import (
 	"encoding/json"
-	"github.com/koofr/go-httpclient"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
+
+	"github.com/koofr/go-httpclient"
 )
 
 const (
@@ -24,55 +26,74 @@ type RefreshRespError struct {
 }
 
 type OneDriveAuth struct {
-	ClientId     string
-	ClientSecret string
-	RedirectUri  string
-	AccessToken  string
-	RefreshToken string
-	ExpiresAt    time.Time
+	ClientId       string
+	ClientSecret   string
+	RedirectUri    string
+	AccessToken    string
+	RefreshToken   string
+	ExpiresAt      time.Time
+	OnTokenRefresh func()
+
+	mutex sync.Mutex
 }
 
 func (a *OneDriveAuth) ValidToken() (token string, err error) {
 	if time.Now().Unix() > a.ExpiresAt.Unix() {
-		data := url.Values{}
-		data.Set("grant_type", "refresh_token")
-		data.Set("client_id", a.ClientId)
-		data.Set("client_secret", a.ClientSecret)
-		data.Set("redirect_uri", a.RedirectUri)
-		data.Set("refresh_token", a.RefreshToken)
-
-		var respVal RefreshResp
-
-		_, err = httpclient.DefaultClient.Request(&httpclient.RequestData{
-			Method:         "POST",
-			FullURL:        "https://login.live.com/oauth20_token.srf",
-			ExpectedStatus: []int{http.StatusOK},
-			ReqEncoding:    httpclient.EncodingForm,
-			ReqValue:       data,
-			RespEncoding:   httpclient.EncodingJSON,
-			RespValue:      &respVal,
-		})
-
+		err = a.UpdateRefreshToken()
 		if err != nil {
-			err = HandleError(err)
-
-			if ode, ok := IsOneDriveError(err); ok {
-				refreshErr := &RefreshRespError{}
-				if jsonErr := json.Unmarshal([]byte(ode.Err.Message), &refreshErr); jsonErr == nil {
-					ode.Err.Code = refreshErr.Error
-					ode.Err.Message = refreshErr.ErrorDescription
-				}
-			}
-
 			return "", err
 		}
-
-		a.AccessToken = respVal.AccessToken
-		a.RefreshToken = respVal.RefreshToken
-		a.ExpiresAt = time.Now().Add(time.Duration(respVal.ExpiresIn) * time.Second)
 	}
 
 	token = a.AccessToken
 
 	return token, nil
+}
+
+func (a *OneDriveAuth) UpdateRefreshToken() (err error) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
+	data := url.Values{}
+	data.Set("grant_type", "refresh_token")
+	data.Set("client_id", a.ClientId)
+	data.Set("client_secret", a.ClientSecret)
+	data.Set("redirect_uri", a.RedirectUri)
+	data.Set("refresh_token", a.RefreshToken)
+
+	var respVal RefreshResp
+
+	_, err = httpclient.DefaultClient.Request(&httpclient.RequestData{
+		Method:         "POST",
+		FullURL:        "https://login.live.com/oauth20_token.srf",
+		ExpectedStatus: []int{http.StatusOK},
+		ReqEncoding:    httpclient.EncodingForm,
+		ReqValue:       data,
+		RespEncoding:   httpclient.EncodingJSON,
+		RespValue:      &respVal,
+	})
+
+	if err != nil {
+		err = HandleError(err)
+
+		if ode, ok := IsOneDriveError(err); ok {
+			refreshErr := &RefreshRespError{}
+			if jsonErr := json.Unmarshal([]byte(ode.Err.Message), &refreshErr); jsonErr == nil {
+				ode.Err.Code = refreshErr.Error
+				ode.Err.Message = refreshErr.ErrorDescription
+			}
+		}
+
+		return err
+	}
+
+	a.AccessToken = respVal.AccessToken
+	a.RefreshToken = respVal.RefreshToken
+	a.ExpiresAt = time.Now().Add(time.Duration(respVal.ExpiresIn) * time.Second)
+
+	if a.OnTokenRefresh != nil {
+		a.OnTokenRefresh()
+	}
+
+	return nil
 }
